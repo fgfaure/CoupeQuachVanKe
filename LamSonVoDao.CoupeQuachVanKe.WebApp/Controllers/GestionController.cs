c5493b5ae243a85b087cec72f53fd7c2be96a367
fgfaure <fgfaure@gmail.com>
2015-05-02 12:27:00 +0200

    oublie d'ajout ..

---
+++ LamSonVoDao.CoupeQuachVanKe.WebApp/Controllers/GestionController.cs
@@ -0,0 +1,89 @@
+﻿using LamSonVoDao.CoupeQuachVanKe.AccesPattern;
+using LamSonVoDao.CoupeQuachVanKe.DataTransferOjbect;
+using LamSonVoDao.CoupeQuachVanKe.DataTransferOjbect.Enumerations;
+using Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LamSonVoDao.CoupeQuachVanKe.WebApp.Controllers
+{
+    public class GestionController : Controller
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
+        // GET: Gestion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetDetails(string id)
+        {
+            int parsed;
+
+            if (int.TryParse(id, out parsed))
+            {
+                var participations = this.unitOfWork.Repository<Participation>().Read();
+                var epreuves = this.unitOfWork.Repository<Epreuve>().Read(e => e.Statut == (StatutEpreuve)parsed);
+                var query = from epreuve in epreuves
+                            select new { Nom = epreuve.Nom, Participants = (epreuve.Participations != null && epreuve.Participations.Count > 0) ? epreuve.Participations.Count : 0 };
+                var result = new JsonResult();
+                result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+                result.Data = query.ToList();
+                return result;
+
+            }
+            else
+            {
+                throw new ArgumentException("parameter is not valid.");
+            }
+        }
+        [HttpGet]
+        public JsonResult GetEpreuveStatut()
+        {
+            var result = new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            var epreuvesRepo = this.unitOfWork.Repository<Epreuve>();
+
+            var epreuves = epreuvesRepo.Read();
+
+            var counted = from epreuve in epreuves
+                          group epreuve by epreuve.Statut into res
+                          orderby res.Key
+                          select new { Statut = res.Key, EpreuveStatut = StatutEpreuves.ResourceManager.GetString(((StatutEpreuve)res.Key).ToString()), Count = res.Count(), Couleur = ColorChoice((StatutEpreuve)res.Key) };
+            result.Data = counted.OrderBy(a => a.EpreuveStatut);
+            return result;
+        }
+
+        private string ColorChoice(StatutEpreuve statutEpreuve)
+        {
+            var result = "#b12b2b";
+
+            switch (statutEpreuve)
+            {
+                case StatutEpreuve.Assignee:
+                    result = "#ff8300";
+                    break;
+                case StatutEpreuve.EnCours:
+                    result = "#0011de";
+                    break;
+                case StatutEpreuve.Fermee:
+                    result = "#ff0000";
+                    break;
+                case StatutEpreuve.Prete:
+                    result = "#FFFF66";
+                    break;
+                case StatutEpreuve.Terminee:
+                    result = "#00de00";
+                    break;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ LamSonVoDao.CoupeQuachVanKe.WebApp/Controllers/ResultatController.cs
@@ -0,0 +1,121 @@
+﻿using LamSonVoDao.CoupeQuachVanKe.AccesPattern;
+using LamSonVoDao.CoupeQuachVanKe.DataTransferOjbect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LamSonVoDao.CoupeQuachVanKe.WebApp.Helper;
+using LamSonVoDao.CoupeQuachVanKe.WebApp.Models.Coupe;
+
+namespace LamSonVoDao.CoupeQuachVanKe.WebApp.Controllers
+{
+    public class ResultatController : Controller
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
+        private Repository<Club> clubsRepository;
+
+        private Repository<Participant> participantsRepository;
+
+        private Repository<Participation> participationsRepository;
+
+        private Repository<Resultat> resultatsRepository;
+
+        private Repository<Epreuve> epreuvesRepository;
+
+        public ResultatController()
+        {
+            this.clubsRepository = this.unitOfWork.Repository<Club>();
+            this.participantsRepository = this.unitOfWork.Repository<Participant>();
+            this.participationsRepository = this.unitOfWork.Repository<Participation>();
+            this.resultatsRepository = this.unitOfWork.Repository<Resultat>();
+            this.epreuvesRepository = this.unitOfWork.Repository<Epreuve>();
+        }
+
+        // GET: Resultat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetClubs()
+        {
+            JsonResult result = new JsonResult();
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            result.Data = this.clubsRepository.Read().Select(c => c.ToModel());
+            return result;
+        }
+
+        public JsonResult GetCompetiteursFromClub(string id)
+        {
+            int parsed = 0;
+
+            if (int.TryParse(id, out parsed))
+            {
+                if (parsed == 0)
+                {
+                    throw new ArgumentException("id can't be 0.");
+                }
+
+                var result = new JsonResult();
+                result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+                var club = this.clubsRepository.Read().FirstOrDefault(c => c.Id == parsed);
+                var competiteurs = this.participantsRepository.Read().Where(p => p.ClubId == club.Id);
+
+                result.Data = competiteurs.Select(c => c.ToModel());
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("id is not valid.");
+            }
+        }
+
+        public JsonResult GetResultatsFromCompetiteur(string id)
+        {
+            int parsed = 0;
+
+            if (int.TryParse(id, out parsed))
+            {
+                if (parsed == 0)
+                {
+                    throw new ArgumentException("id can't be 0.");
+                }
+
+                var result = new JsonResult();
+                result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+
+                var resultats = this.resultatsRepository.Read();
+                var epreuves = this.epreuvesRepository.Read();
+                var competiteur = this.participantsRepository.Read().FirstOrDefault(p => p.Id == parsed);
+                var participations = this.participationsRepository.Read().Where(p => p.ParticipantId == competiteur.Id);
+                var data = new List<ResultatModel>();
+
+                foreach (var participation in participations)
+                {
+                    data.Add(new ResultatModel
+                    {
+                        EpreuveId = participation.EpreuveId,
+                        Epreuve = participation.Epreuve.Nom,
+                        ResultatId = participation.ResultatId,
+                        ParticipantId = participation.ParticipantId,
+                        Nom = competiteur.Nom,
+                        Prenom = competiteur.Prenom,
+                        Score = participation.Resultat.Score,
+                        Classement = participation.Resultat.Classement,
+                        Abandon = participation.Resultat.Abandon,
+                        Absence = participation.Resultat.Absence,
+                    });
+                }
+
+                result.Data = data;
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("id is not valid.");
+            }
+        }
+    }
+}
